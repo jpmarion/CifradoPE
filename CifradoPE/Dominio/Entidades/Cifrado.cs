bea620fe9e7ed4b5248559f894003245a67ac502
Juan Pablo <totomarion@gmail.com>
2023-06-10 11:42:36 -0300

    Crear cifrado

---
+++ CifradoPE/Dominio/Entidades/Cifrado.cs
@@ -0,0 +1,11 @@
+﻿using CifradoPE.Dominio.Entidades.Interface;
+
+namespace CifradoPE.Dominio.Entidades
+{
+        internal class Cifrado : ICifrado
+    {
+        public string Texto { get; set; }
+        public string Key { get; set; }
+        public string PrivateKet { get; set; }
+    }
+}
---
+++ CifradoPE/Dominio/Inteface/ICifrado.cs
@@ -0,0 +1,9 @@
+﻿namespace CifradoPE.Dominio.Inteface
+{
+    internal interface ICifrado
+    {
+        string Key { get; set; }
+        string PrivateKet { get; set; }
+        string Texto { get; set; }
+    }
+}
